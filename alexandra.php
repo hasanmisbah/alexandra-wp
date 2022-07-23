9ace8ae27ee317ca7d3a06a12a1faeaea58c1b30
Hasan Misbah <hasanmisbah01@gmail.com>
2022-07-23 17:53:49 +0600

    Comment updated

--- alexandra.php
+++ alexandra.php
@@ -14,9 +14,15 @@
 /**
  * Plugin Name:       Alexandra
  * Plugin URI:        https://example.com/plugin-name
- * Description:       Learning plugin Development || (The only plugin you need in WordPress to Build Rocket launcher or
- * atom bomb) Version:           1.0.0 Requires at least: 5.2 Requires PHP:      8.0 Author:            Hasan Misbah
- * Author URI:        https://github.com/hasanmisbah Text Domain:       alexandra License:           GPL v2 or later
+ * Description:       Learning plugin Development (The only plugin you need in WordPress to Build Rocket launcher or
+ * atom bomb)
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      8.0
+ * Author:            Hasan Misbah
+ * Author URI:        https://github.com/hasanmisbah
+ * Text Domain:       alexandra
+ * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
