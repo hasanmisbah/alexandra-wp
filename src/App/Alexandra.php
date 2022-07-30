e9ce9085b8b97269e513c1d7683f6196bfe52278
Hasan Misbah <hasanmisbah01@gmail.com>
2022-07-30 17:54:32 +0600

    Code Formatted

--- src/App/Alexandra.php
+++ src/App/Alexandra.php
@@ -8,9 +8,19 @@ use Alexandra\App\Trait\ApplicationContract;
 class Alexandra extends Controller
 {
     use ApplicationContract;
+
     public array $modules = [
         \Alexandra\App\Module\Admin::class,
         \Alexandra\App\Module\Test::class,
+    ];
+
+    protected array $modulerData = [
+
+        // :TODO: Structure this data in a way that it can be used to register modules
+        'module_slug' => [
+            'module' => 'module_class',
+            'title' => 'Module Title',
+        ],
     ];
 
     private Alexandra|null $instance = null;
