7e68a994a11d66c7c9a2ad8fe17e0cc9c5f8281b
Hasan Misbah <hasanmisbah01@gmail.com>
2022-08-10 15:01:26 +0600

    Contact detailst view done

--- src/resource/js/Components/Contact/ContactDetails.vue
+++ src/resource/js/Components/Contact/ContactDetails.vue
@@ -1,21 +1,29 @@
 <template>
   <Modal v-model="state.showModal" title="Contact Details" hide-footer>
-    <table class="el-table" style="width: 100%">
+    <table class="contact-table" style="width: 100%">
       <tbody>
         <tr>
-          <th>Name</th>
-          <td> {{ state.contact.name }} </td>
+          <th scope="row">
+            Name
+          </th>
+          <td> {{ state.contact.name }}</td>
         </tr>
         <tr>
-          <th>Phone</th>
+          <th scope="row">
+            Phone
+          </th>
           <td>{{ state.contact.phone }}</td>
         </tr>
         <tr>
-          <th>Email</th>
+          <th scope="row">
+            Email
+          </th>
           <td>{{ state.contact.email }}</td>
         </tr>
         <tr>
-          <th>Notes</th>
+          <th scope="row">
+            Message
+          </th>
           <td>{{ state.contact.message }}</td>
         </tr>
       </tbody>
@@ -91,6 +99,32 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+.contact-table {
+  display: table;
+
+  tr {
+    display: table-row;
+    width: 100%;
+
+    th, td {
+      display: table-cell;
+      padding: 0.5rem;
+      text-align: left;
+      vertical-align: middle;
+      width: auto;
+    }
+
+    th {
+      width: 15%;
+    }
+
+    td {
+      width: 75%;
+      border-bottom: 1px solid #e3e3e3;
+    }
+  }
+
+}
 </style>
