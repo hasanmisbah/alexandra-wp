5876c91b11b59041937123f4beaaf6d22544c70f
Hasan Misbah <hi@hasanmisbah.com>
2022-08-16 12:47:51 +0600

    Avatar Image Updated

--- src/resource/view/contactShortcodeView.php
+++ src/resource/view/contactShortcodeView.php
@@ -1,6 +1,6 @@
 <div class="user-profile">
     <img class="avatar"
-         src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTF_erFD1SeUnxEpvFjzBCCDxLvf-wlh9ZuPMqi02qGnyyBtPWdE-3KoH3s"
+         src="https://p.kindpng.com/picc/s/22-223941_transparent-avatar-png-male-avatar-icon-transparent-png.png"
          />
     <div class="username"><?php echo $data->name ?></div>
     <div class="bio"><?php echo $data->phone ?></div>
